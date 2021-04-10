6a35dfb167426c91980451931a8194cf8651e7ff
Abhishek Paudel <abpaudel@users.noreply.github.com>
2021-04-10 13:53:30 -0400

    update models for custom architecture

--- model.py
+++ model.py
@@ -1,63 +1,45 @@
 import torch
-from torch.nn import Linear
+from torch.nn import Linear, Sequential
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, SAGEConv
+from torch_geometric.nn import SAGEConv, GATConv, GCNConv, TAGConv
 
-class GraphSAGE(torch.nn.Module):
-    def __init__(self):
-        super(GraphSAGE, self).__init__()
+
+class GNN(torch.nn.Module):
+
+    def __init__(self, Conv=SAGEConv, n_hidden=2):
+        super(GNN, self).__init__()
         torch.manual_seed(42)
-        self.conv1 = SAGEConv(4, 10)
-        self.conv2 = SAGEConv(10, 20)
-        self.conv3 = SAGEConv(20, 20)
-        self.classifier = Linear(20, 8)
+        self.layer1 = Conv(6, 16)
+        self.layer2 = torch.nn.ModuleList()
+        for _ in range(n_hidden-1):
+            self.layer2.append(Conv(16,16))
+        self.classifier = Linear(16, 8)
 
     def forward(self, x, edge_index):
-        h = self.conv1(x, edge_index)
+        h = self.layer1(x, edge_index)
         h = F.relu(h)
-        h = self.conv2(h, edge_index)
-        h = F.relu(h)
-        h = self.conv3(h, edge_index)
-        h = F.relu(h) 
+        for layer in self.layer2:
+            h = layer(h, edge_index)
+            h = F.relu(h)
         out = self.classifier(h)
         return out
-
-
-class GCN(torch.nn.Module):
-    def __init__(self):
-        super(GCN, self).__init__()
-        torch.manual_seed(42)
-        self.conv1 = GCNConv(4, 10)
-        self.conv2 = GCNConv(10, 20)
-        self.conv3 = GCNConv(20, 20)
-        self.classifier = Linear(20, 8)
-
-    def forward(self, x, edge_index):
-        h = self.conv1(x, edge_index)
-        h = F.relu(h)
-        h = self.conv2(h, edge_index)
-        h = F.relu(h)
-        h = self.conv3(h, edge_index)
-        h = F.relu(h) 
-        out = self.classifier(h)
-        return out
-
 
 class NN(torch.nn.Module):
-    def __init__(self):
+
+    def __init__(self, Conv=Linear, n_hidden=2):
         super(NN, self).__init__()
         torch.manual_seed(42)
-        self.layer1 = Linear(4, 10)
-        self.layer2 = Linear(10, 20)
-        self.layer3 = Linear(20, 20)
-        self.classifier = Linear(20, 8)
+        self.layer1 = Linear(6, 16)
+        self.layer2 = torch.nn.ModuleList()
+        for _ in range(n_hidden-1):
+            self.layer2.append(Conv(16,16))
+        self.classifier = Linear(16, 8)
 
     def forward(self, x, edge_index):
         h = self.layer1(x)
         h = F.relu(h)
-        h = self.layer2(h)
-        h = F.relu(h)
-        h = self.layer3(h)
-        h = F.relu(h) 
+        for layer in self.layer2:
+            h = layer(h)
+            h = F.relu(h)
         out = self.classifier(h)
         return out
