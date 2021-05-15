3aaf23ad0cfd504efb3d65e2930ff22e7e55edc7
Abhishek Paudel <abpaudel@users.noreply.github.com>
2021-05-15 16:05:12 -0400

    add parser and clean up

--- train.py
+++ train.py
@@ -2,55 +2,61 @@ import torch
 import numpy as np
 from dataset import FloorplanGraphDataset
 from utils import  accuracy
-from model import GNN, NN
+from model import Model
+from torch.nn import Linear
 from torch_geometric.data import DataLoader
 from torch_geometric.nn import SAGEConv, GATConv, GCNConv, TAGConv
+import pathlib
+import argparse
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--model', choices=['mlp', 'gcn', 'gat', 'sage', 'tagcn'], default='sage', help='Type of model')
+    parser.add_argument('--hidden', type=int, default=2, help='Number of hidden/messsage passing layers')
+    parser.add_argument('--epoch', type=int, default=100, help='Number of epochs to train')
+    parser.add_argument('--lr', type=float, default=0.004, help='Learning rate')
+    parser.add_argument('--step', type=int, default=10, help='Step size for exponential learning rate scheduling')
+    parser.add_argument('--gamma', type=float, default=0.8, help='Decay rate for exponential learning rate scheduling')
+    parser.add_argument('--bs', type=int, default=128, help='Batch size for training')
+    parser.add_argument('--outpath', type=str, default='./results', help='Path to save results')
+    args = parser.parse_args()
+    models = {
+                'mlp': Linear,
+                'gcn': GCNConv,
+                'gat': GATConv,
+                'sage': SAGEConv,
+                'tagcn': TAGConv,
+            }
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-print(device)
+    outpath = pathlib.Path(args.outpath)
+    outpath.mkdir(parents=True, exist_ok=True) 
 
+    torch.manual_seed(42)
+    model = Model(layer_type=models[args.model], n_hidden=args.hidden)
+    print(model)
+    model = model.to(device)
 
-outpath = '/scratch/apaudel4/gnn/results/'
-# outpath = './'
-torch.manual_seed(42)
+    dataset = FloorplanGraphDataset(path='./data/housegan_clean_data.npy', split=None)
+    train = [dataset[i].to(device) for i in range(120000)]
+    trainloader = DataLoader(train, batch_size=args.bs, shuffle=True)
+    trainloader2 = DataLoader(train, batch_size=120000)
+    test = [dataset[i].to(device) for i in range(120000,143184)]
+    testloader = DataLoader(test, batch_size=23184)
 
-dataset = FloorplanGraphDataset(path='./data/housegan_clean_data.npy', split=None)
-# train = FloorplanGraphDataset(path='housegan_clean_data.npy', split='train')
-train = [dataset[i].to(device) for i in range(120000)]
-trainloader = DataLoader(train, batch_size=128, shuffle=True)
-trainloader2 = DataLoader(train, batch_size=120000)
-# test = FloorplanGraphDataset(path='housegan_clean_data.npy', split='test')
-test = [dataset[i].to(device) for i in range(120000,143184)]
-testloader = DataLoader(test, batch_size=23184)
+    num_epochs = args.epoch
+    lr = args.lr
+    step_size = args.step
+    gamma = args.gamma
 
-Conv = [SAGEConv, GCNConv, GATConv, TAGConv]
-models = []
-
-hid_r = 2,7
-
-for n_h in range(*hid_r):
-    for C in Conv:
-        models.append(GNN(Conv=C, n_hidden=n_h))
-for n_h in range(*hid_r):
-    models.append(NN(n_hidden=n_h))
-
-
-lr = 0.004
-step_size = 10
-gamma = 0.8
-
-for model in models:
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-    model = model.to(device)
-    num_epochs = 100
     loss_ep = []
     te_acc_ep = []
     tr_acc_ep = []
-    print(model)
     
     for epoch in range(num_epochs):
         model.train()
@@ -73,6 +79,6 @@ for model in models:
         print(f'Epoch [{epoch+1}/{num_epochs}] Loss: {loss:.10f}, Train Acc: {tr_acc:.6f}, Test Acc: {te_acc:.6f}')
         
     result = np.array([loss_ep, tr_acc_ep, te_acc_ep]).T
-    np.savetxt(f'{outpath}{type(model.layer1).__name__}{len(model.layer2)+1}_loss_tracc_teacc_{lr}_{num_epochs}_{step_size}_{gamma}.txt', result)
+    np.savetxt(outpath/f'{type(model.layer1).__name__}{len(model.layer2)+1}_loss_tracc_teacc_{lr}_{num_epochs}_{step_size}_{gamma}.txt', result)
     max_idx = result[:,2].argmax()
     print(f'\nMax Test Accuracy at Epoch {max_idx+1}: {result[max_idx]}\n')
