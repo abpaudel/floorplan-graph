54f613c2c0e2e621993fc8159d086035329d6259
Abhishek Paudel <abpaudel@users.noreply.github.com>
2021-05-15 16:03:53 -0400

    one model to rule them all

--- model.py
+++ model.py
@@ -1,45 +1,24 @@
 import torch
-from torch.nn import Linear, Sequential
+from torch.nn import Linear
 import torch.nn.functional as F
-from torch_geometric.nn import SAGEConv, GATConv, GCNConv, TAGConv
 
+class Model(torch.nn.Module):
 
-class GNN(torch.nn.Module):
-
-    def __init__(self, Conv=SAGEConv, n_hidden=2):
-        super(GNN, self).__init__()
+    def __init__(self, layer_type, n_hidden=2):
+        super(Model, self).__init__()
         torch.manual_seed(42)
-        self.layer1 = Conv(6, 16)
+        self.is_mlp = True if layer_type.__name__=='Linear' else False
+        self.layer1 = layer_type(6, 16)
         self.layer2 = torch.nn.ModuleList()
         for _ in range(n_hidden-1):
-            self.layer2.append(Conv(16,16))
+            self.layer2.append(layer_type(16,16))
         self.classifier = Linear(16, 8)
 
     def forward(self, x, edge_index):
-        h = self.layer1(x, edge_index)
+        h = self.layer1(x) if self.is_mlp else self.layer1(x, edge_index)
         h = F.relu(h)
         for layer in self.layer2:
-            h = layer(h, edge_index)
-            h = F.relu(h)
-        out = self.classifier(h)
-        return out
-
-class NN(torch.nn.Module):
-
-    def __init__(self, Conv=Linear, n_hidden=2):
-        super(NN, self).__init__()
-        torch.manual_seed(42)
-        self.layer1 = Linear(6, 16)
-        self.layer2 = torch.nn.ModuleList()
-        for _ in range(n_hidden-1):
-            self.layer2.append(Conv(16,16))
-        self.classifier = Linear(16, 8)
-
-    def forward(self, x, edge_index):
-        h = self.layer1(x)
-        h = F.relu(h)
-        for layer in self.layer2:
-            h = layer(h)
+            h = layer(h) if self.is_mlp else layer(h, edge_index)
             h = F.relu(h)
         out = self.classifier(h)
         return out
